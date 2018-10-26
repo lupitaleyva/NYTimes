da16a44d7f84a11c58ebd51977417626a1451f61
Jessa Molyneux <jessa@refinedrisk.com>
2018-10-25 21:18:12 -0400

    merging

---
+++ javascript/nytcode.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){ 
+
+    $("#formGroupExampleInput").on("click", function(){
+        console.log("")
+    });
+
+    $("#exampleFormControlSelect1").on("click", function(){
+        console.log("")
+    });
+    
+
+    $("formGroupExampleInput1").on("click", function(){
+        console.log("")
+    
+    });
+
+    $("formGroupExampleInput2").on("click", function(){
+        console.log("")
+    
+    });
+
+    $("formGroupExampleInput3").on("click", function(){
+        console.log("")
+    
+    });
+    
+
+
+})
