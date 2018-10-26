28f070ae404c5fc0e6071836a22388aa323649ff
Jessa Molyneux <jessa@refinedrisk.com>
2018-10-25 21:23:00 -0400

    new changes

--- javascript/nytcode.js
+++ javascript/nytcode.js
@@ -25,5 +25,27 @@ $(document).ready(function(){
     });
     
 
+    var apiKey= "7ca76b827f864a05a9b30bd1ccf9b78a";
+
+    var searchTerm= "iphone";
+    var recordsToRetrieve="2";
+    var startYear="2017";
+    var endYear="2018";
+
+
+    var queryURL="https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ searchTerm + "&api-key="+apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+
+ 
+
+
+// Storing the rating data
+var parameters = response.searchTerm;
+
 
 })
